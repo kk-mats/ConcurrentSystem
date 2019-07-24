efefd47da451183175bb6bfa1d637a444a093f3e
kk-mats <k.mats.kk@gmail.com>
2019-07-24 18:05:10 +0900

    first commit

---
+++ src/concurrentSystems/HelloAgent.java
@@ -0,0 +1,12 @@
+package concurrentSystems;
+
+import jade.core.Agent;
+
+public class HelloAgent extends Agent
+{
+	protected void setup()
+	{
+		System.out.println("Hello World.");
+		System.out.println("My name is "+this.getLocalName());
+	}
+}
